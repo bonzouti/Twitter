0fc3c541dfa5b0f5e13de76a8dd91167acfc2389
pierremathieu18@gmail.com <mathieupierre@MacBook-Air-de-MATHIEU.local>
2019-01-16 19:47:45 +0100

    modification de l'application Twitter

--- lib/Twitter.rb
+++ lib/Twitter.rb
@@ -13,7 +13,6 @@ client = Twitter::REST::Client.new do |config|
   config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
   config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
   config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
- 
   end
 end
 
@@ -25,28 +24,27 @@ def login_streaming
     config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
     config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
   end
-  client 
 end
   #mon premier tweet
 #login_twitter.update("Mon robot fonctionne parfaitement")
 
-# Fonction qui sélection 5 journalites au hasard pour leur envoyer un tweet
-# def bonjour
-#   journalists.sample(5).each do |i|
-#   login_twitter.update("Bonjour #{i}! Félicitations pour votre travail,@the_hackin_pro #bonjour_monde")
-#   end 
-# end
+# 2.1 Fonction qui sélection 5 journalites au hasard pour leur envoyer un tweet
+def bonjour
+  journalists.sample(5).each do |i|
+  login_twitter.update("Bonjour #{i}! Félicitations pour votre travail,@the_hackin_pro #bonjour_monde")
+  end 
+end
 
-# #Fonction qui affiche les 20 tweets récents contenant le hashtag #bonjour_monde
-# def likez_bonjour
-#      login_twitter.search("#bonjour_monde", result_type: "recent").take(2).each do |tweet|
-#      login_twitter.fav tweet
-#      end
+# 2.2 Fonction qui va likez 20 tweets récents contenant le hashtag #bonjour_monde
+def likez_bonjour
+  login_twitter.search("#bonjour_monde", result_type: "recent").take(5).each do |tweet|
+    puts "Like"
+  login_twitter.fav(tweet.user)
+  end
+ end
 
-#  end
-# likez_bonjour
 
-#Fonction qui va follower 5 follower
+# 2.3 Fonction qui va suivre tweets récents contenant le hashtag #Macron
 def follow_user
   login_twitter.search("#Macron", result_type: "recent").take(5).each do |tweet| 
     puts "Youpi"
@@ -54,16 +52,19 @@ def follow_user
 
   end 
 end
-follow_user
+
  
 
-# Fonction qui surveille le hashtag #bonjour_monde en temps réel et qui like automatiquement les tweets
-# def stream
-#   login_streaming.filter(track: "#Macron") do |tweet| puts "il l'a fait !!!"
-#     if tweet.is_a?(Twitter::Tweet)
-#       login_twitter.fav(tweet)
-#     end
-#   end
+#2.4 Fonction qui surveille le hashtag #bonjour_monde en temps réel et qui like automatiquement les tweets
+def stream
+  login_streaming.filter(track: "#Macron") do |tweet| puts "il l'a fait !!!"
+    if tweet.is_a?(Twitter::Tweet)
+      login_twitter.fav(tweet)
+    end
+  end
   
-# end
-# stream 
+end
+login_twitter
+likez_bonjour
+follow_user
+stream 
